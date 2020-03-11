ce6b09511a882c3f5d0dab4c0a05944cf590d688
miki <miki.ishii16@gmail.com>
2020-03-11 11:35:54 +0900

    modal機能を追加

---
+++ main.js
@@ -0,0 +1,12 @@
+var $ = require('jquery');
+
+$(function () {
+  $('.js-modal--open').on('click', function () {
+    $('.js-modal').fadeIn();
+    return false;
+  });
+  $('.js-modal--close').on('click', function () {
+    $('.js-modal').fadeOut();
+    return false;
+  });
+});
